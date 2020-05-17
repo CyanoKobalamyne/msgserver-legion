6a73a228c63d967b0f39f81ae74a101ccb6328a8
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2020-05-17 18:02:38 -0400

    Compact operations over channels into loops

--- messaging.cc
+++ messaging.cc
@@ -17,25 +17,13 @@ enum TaskID {
     EXECUTE_POST_TASK,
 };
 
-enum UserFieldID {
-    FOLLOWED_CHANNEL_ID_1,
-    FOLLOWED_CHANNEL_ID_2,
-    FOLLOWED_CHANNEL_ID_3,
-    FOLLOWED_CHANNEL_ID_4,
-    FOLLOWED_CHANNEL_ID_5,
-};
+enum UserFieldID { FOLLOWED_CHANNEL_IDS };
 
 enum ChannelFieldID {
     NEXT_MSG_ID,
 };
 
-enum UserFirstUnreadFieldID {
-    NEXT_UNREAD_MSG_ID_1,
-    NEXT_UNREAD_MSG_ID_2,
-    NEXT_UNREAD_MSG_ID_3,
-    NEXT_UNREAD_MSG_ID_4,
-    NEXT_UNREAD_MSG_ID_5,
-};
+enum UserFirstUnreadFieldID { NEXT_UNREAD_MSG_IDS };
 
 enum MessageFieldID {
     AUTHOR_ID,
@@ -43,6 +31,7 @@ enum MessageFieldID {
     TEXT,
 };
 
+constexpr unsigned int CHANNELS_PER_USER = 5;
 constexpr unsigned int MESSAGE_LENGTH = 256;
 
 typedef uint16_t user_id_t;
@@ -58,19 +47,19 @@ typedef struct {
 
 typedef struct {
     user_id_t user_id;
-    channel_id_t watched_channel_ids[5];
+    channel_id_t watched_channel_ids[CHANNELS_PER_USER];
 } PrepareFetchData;
 
 typedef struct {
-    message_id_t next_unread_msg_ids[5];
-    message_id_t next_channel_msg_ids[5];
+    message_id_t next_unread_msg_ids[CHANNELS_PER_USER];
+    message_id_t next_channel_msg_ids[CHANNELS_PER_USER];
 } PrepareFetchResponse;
 
 typedef struct {
     user_id_t user_id;
-    channel_id_t watched_channel_ids[5];
-    message_id_t next_unread_msg_ids[5];
-    message_id_t next_channel_msg_ids[5];
+    channel_id_t watched_channel_ids[CHANNELS_PER_USER];
+    message_id_t next_unread_msg_ids[CHANNELS_PER_USER];
+    message_id_t next_channel_msg_ids[CHANNELS_PER_USER];
 } ExecuteFetchData;
 
 typedef struct {
@@ -176,52 +165,32 @@ void dispatch_task(const Task *task,
     FieldSpace user_fields = runtime->create_field_space(ctx);
     FieldAllocator allocator =
         runtime->create_field_allocator(ctx, user_fields);
-    allocator.allocate_field(sizeof(channel_id_t), FOLLOWED_CHANNEL_ID_1);
-    allocator.allocate_field(sizeof(channel_id_t), FOLLOWED_CHANNEL_ID_2);
-    allocator.allocate_field(sizeof(channel_id_t), FOLLOWED_CHANNEL_ID_3);
-    allocator.allocate_field(sizeof(channel_id_t), FOLLOWED_CHANNEL_ID_4);
-    allocator.allocate_field(sizeof(channel_id_t), FOLLOWED_CHANNEL_ID_5);
+    allocator.allocate_field(CHANNELS_PER_USER * sizeof(channel_id_t),
+                             FOLLOWED_CHANNEL_IDS);
     LogicalRegionT<1> users =
         runtime->create_logical_region(ctx, user_ids, user_fields);
     // Initialize array.
     RegionRequirement user_init_req(users, WRITE_DISCARD, EXCLUSIVE, users);
-    user_init_req.add_field(FOLLOWED_CHANNEL_ID_1);
-    user_init_req.add_field(FOLLOWED_CHANNEL_ID_2);
-    user_init_req.add_field(FOLLOWED_CHANNEL_ID_3);
-    user_init_req.add_field(FOLLOWED_CHANNEL_ID_4);
-    user_init_req.add_field(FOLLOWED_CHANNEL_ID_5);
+    user_init_req.add_field(FOLLOWED_CHANNEL_IDS);
     InlineLauncher user_init_launcher(user_init_req);
     PhysicalRegion init_region = runtime->map_region(ctx, user_init_launcher);
-    const FieldAccessor<WRITE_DISCARD, channel_id_t, 1> channel_id_1(
-        init_region, FOLLOWED_CHANNEL_ID_1);
-    const FieldAccessor<WRITE_DISCARD, channel_id_t, 1> channel_id_2(
-        init_region, FOLLOWED_CHANNEL_ID_2);
-    const FieldAccessor<WRITE_DISCARD, channel_id_t, 1> channel_id_3(
-        init_region, FOLLOWED_CHANNEL_ID_3);
-    const FieldAccessor<WRITE_DISCARD, channel_id_t, 1> channel_id_4(
-        init_region, FOLLOWED_CHANNEL_ID_4);
-    const FieldAccessor<WRITE_DISCARD, channel_id_t, 1> channel_id_5(
-        init_region, FOLLOWED_CHANNEL_ID_5);
+    const FieldAccessor<WRITE_DISCARD, channel_id_t *, 1> channel_id_mem(
+        init_region, FOLLOWED_CHANNEL_IDS);
     std::vector<int> all_channel_ids(channel_count);
     std::iota(all_channel_ids.begin(), all_channel_ids.end(), 0);
     for (PointInRectIterator<1> iter(user_id_range); iter(); iter++) {
         std::random_shuffle(all_channel_ids.begin(), all_channel_ids.end());
-        channel_id_1[*iter] = all_channel_ids[0];
-        channel_id_2[*iter] = all_channel_ids[1];
-        channel_id_3[*iter] = all_channel_ids[2];
-        channel_id_4[*iter] = all_channel_ids[3];
-        channel_id_5[*iter] = all_channel_ids[4];
+        for (unsigned int i = 0; i < CHANNELS_PER_USER; i++) {
+            channel_id_mem[*iter][i] = all_channel_ids[i];
+        }
     }
     // Leave this region mapped.
 
     /* Next unread array. */
     FieldSpace next_unread_fields = runtime->create_field_space(ctx);
     allocator = runtime->create_field_allocator(ctx, next_unread_fields);
-    allocator.allocate_field(sizeof(message_id_t), NEXT_UNREAD_MSG_ID_1);
-    allocator.allocate_field(sizeof(message_id_t), NEXT_UNREAD_MSG_ID_2);
-    allocator.allocate_field(sizeof(message_id_t), NEXT_UNREAD_MSG_ID_3);
-    allocator.allocate_field(sizeof(message_id_t), NEXT_UNREAD_MSG_ID_4);
-    allocator.allocate_field(sizeof(message_id_t), NEXT_UNREAD_MSG_ID_5);
+    allocator.allocate_field(CHANNELS_PER_USER * sizeof(message_id_t),
+                             NEXT_UNREAD_MSG_IDS);
     LogicalRegionT<1> next_unreads =
         runtime->create_logical_region(ctx, user_ids, next_unread_fields);
     LogicalPartition next_unread_partition =
@@ -229,29 +198,15 @@ void dispatch_task(const Task *task,
     // Initialize array.
     RegionRequirement next_unread_init_req(next_unreads, WRITE_DISCARD,
                                            EXCLUSIVE, next_unreads);
-    next_unread_init_req.add_field(NEXT_UNREAD_MSG_ID_1);
-    next_unread_init_req.add_field(NEXT_UNREAD_MSG_ID_2);
-    next_unread_init_req.add_field(NEXT_UNREAD_MSG_ID_3);
-    next_unread_init_req.add_field(NEXT_UNREAD_MSG_ID_4);
-    next_unread_init_req.add_field(NEXT_UNREAD_MSG_ID_5);
+    next_unread_init_req.add_field(NEXT_UNREAD_MSG_IDS);
     InlineLauncher next_unread_init_launcher(next_unread_init_req);
     init_region = runtime->map_region(ctx, next_unread_init_launcher);
-    const FieldAccessor<WRITE_DISCARD, message_id_t, 1> next_unread_1(
-        init_region, NEXT_UNREAD_MSG_ID_1);
-    const FieldAccessor<WRITE_DISCARD, message_id_t, 1> next_unread_2(
-        init_region, NEXT_UNREAD_MSG_ID_2);
-    const FieldAccessor<WRITE_DISCARD, message_id_t, 1> next_unread_3(
-        init_region, NEXT_UNREAD_MSG_ID_3);
-    const FieldAccessor<WRITE_DISCARD, message_id_t, 1> next_unread_4(
-        init_region, NEXT_UNREAD_MSG_ID_4);
-    const FieldAccessor<WRITE_DISCARD, message_id_t, 1> next_unread_5(
-        init_region, NEXT_UNREAD_MSG_ID_5);
+    const FieldAccessor<WRITE_DISCARD, message_id_t *, 1> next_unread_mem(
+        init_region, NEXT_UNREAD_MSG_IDS);
     for (PointInRectIterator<1> iter(user_id_range); iter(); iter++) {
-        next_unread_1[*iter] = 0;
-        next_unread_2[*iter] = 0;
-        next_unread_3[*iter] = 0;
-        next_unread_4[*iter] = 0;
-        next_unread_5[*iter] = 0;
+        for (unsigned int i = 0; i < CHANNELS_PER_USER; i++) {
+            next_unread_mem[*iter][i] = 0;
+        }
     }
     runtime->unmap_region(ctx, init_region);
 
@@ -313,22 +268,15 @@ void dispatch_task(const Task *task,
                         req.future.get_result<PrepareFetchResponse>();
                     pending_reqs.erase(it);
                     ExecuteFetchData data = {.user_id = req.request.user_id};
-                    data.watched_channel_ids[0] =
-                        channel_id_1[req.request.user_id];
-                    data.watched_channel_ids[1] =
-                        channel_id_2[req.request.user_id];
-                    data.watched_channel_ids[2] =
-                        channel_id_3[req.request.user_id];
-                    data.watched_channel_ids[3] =
-                        channel_id_4[req.request.user_id];
-                    data.watched_channel_ids[4] =
-                        channel_id_5[req.request.user_id];
+                    memcpy(data.watched_channel_ids,
+                           channel_id_mem[req.request.user_id],
+                           sizeof data.watched_channel_ids);
                     memcpy(data.next_channel_msg_ids,
                            response.next_channel_msg_ids,
-                           sizeof response.next_channel_msg_ids);
+                           sizeof data.next_channel_msg_ids);
                     memcpy(data.next_unread_msg_ids,
                            response.next_unread_msg_ids,
-                           sizeof response.next_unread_msg_ids);
+                           sizeof data.next_unread_msg_ids);
                     TaskLauncher launcher(
                         EXECUTE_FETCH_TASK,
                         TaskArgument(&data, sizeof(ExecuteFetchData)));
@@ -336,50 +284,16 @@ void dispatch_task(const Task *task,
                         runtime->get_logical_subregion_by_color(
                             next_unread_partition, req.request.user_id),
                         READ_WRITE, EXCLUSIVE, next_unreads));
-                    launcher.add_field(0, NEXT_UNREAD_MSG_ID_1);
-                    launcher.add_field(1, NEXT_UNREAD_MSG_ID_2);
-                    launcher.add_field(2, NEXT_UNREAD_MSG_ID_3);
-                    launcher.add_field(3, NEXT_UNREAD_MSG_ID_4);
-                    launcher.add_field(4, NEXT_UNREAD_MSG_ID_5);
-                    for (message_id_t i = data.next_unread_msg_ids[0];
-                         i < data.next_channel_msg_ids[0]; i++) {
-                        launcher.add_region_requirement(RegionRequirement(
-                            runtime->get_logical_subregion_by_color(
-                                message_partition, i),
-                            READ_ONLY, EXCLUSIVE, messages));
-                        launcher.add_field(0, NEXT_MSG_ID);
-                    }
-                    for (message_id_t i = data.next_unread_msg_ids[1];
-                         i < data.next_channel_msg_ids[1]; i++) {
-                        launcher.add_region_requirement(RegionRequirement(
-                            runtime->get_logical_subregion_by_color(
-                                message_partition, msg_count + i),
-                            READ_ONLY, EXCLUSIVE, messages));
-                        launcher.add_field(0, NEXT_MSG_ID);
-                    }
-                    for (message_id_t i = data.next_unread_msg_ids[2];
-                         i < data.next_channel_msg_ids[2]; i++) {
-                        launcher.add_region_requirement(RegionRequirement(
-                            runtime->get_logical_subregion_by_color(
-                                message_partition, msg_count * 2 + i),
-                            READ_ONLY, EXCLUSIVE, messages));
-                        launcher.add_field(0, NEXT_MSG_ID);
-                    }
-                    for (message_id_t i = data.next_unread_msg_ids[3];
-                         i < data.next_channel_msg_ids[3]; i++) {
-                        launcher.add_region_requirement(RegionRequirement(
-                            runtime->get_logical_subregion_by_color(
-                                message_partition, msg_count * 3 + i),
-                            READ_ONLY, EXCLUSIVE, messages));
-                        launcher.add_field(0, NEXT_MSG_ID);
-                    }
-                    for (message_id_t i = data.next_unread_msg_ids[4];
-                         i < data.next_channel_msg_ids[4]; i++) {
-                        launcher.add_region_requirement(RegionRequirement(
-                            runtime->get_logical_subregion_by_color(
-                                message_partition, msg_count * 4 + i),
-                            READ_ONLY, EXCLUSIVE, messages));
-                        launcher.add_field(0, NEXT_MSG_ID);
+                    launcher.add_field(0, NEXT_UNREAD_MSG_IDS);
+                    for (unsigned int i = 0; i < CHANNELS_PER_USER; i++) {
+                        for (message_id_t j = data.next_unread_msg_ids[i];
+                             j < data.next_channel_msg_ids[i]; j++) {
+                            launcher.add_region_requirement(RegionRequirement(
+                                runtime->get_logical_subregion_by_color(
+                                    message_partition, i * msg_count + j),
+                                READ_ONLY, EXCLUSIVE, messages));
+                            launcher.add_field(0, NEXT_MSG_ID);
+                        }
                     }
                     executing_reqs.push_back(
                         runtime->execute_task(ctx, launcher));
@@ -431,11 +345,8 @@ void dispatch_task(const Task *task,
         switch (request.action) {
         case FETCH: {
             PrepareFetchData data = {.user_id = request.user_id};
-            data.watched_channel_ids[0] = channel_id_1[request.user_id];
-            data.watched_channel_ids[1] = channel_id_2[request.user_id];
-            data.watched_channel_ids[2] = channel_id_3[request.user_id];
-            data.watched_channel_ids[3] = channel_id_4[request.user_id];
-            data.watched_channel_ids[4] = channel_id_5[request.user_id];
+            memcpy(data.watched_channel_ids, channel_id_mem[request.user_id],
+                   sizeof data.watched_channel_ids);
             TaskLauncher launcher(
                 PREPARE_FETCH_TASK,
                 TaskArgument(&data, sizeof(PrepareFetchData)));
@@ -443,36 +354,14 @@ void dispatch_task(const Task *task,
                 RegionRequirement(runtime->get_logical_subregion_by_color(
                                       next_unread_partition, request.user_id),
                                   READ_ONLY, EXCLUSIVE, next_unreads));
-            launcher.add_field(0, NEXT_UNREAD_MSG_ID_1);
-            launcher.add_field(1, NEXT_UNREAD_MSG_ID_2);
-            launcher.add_field(2, NEXT_UNREAD_MSG_ID_3);
-            launcher.add_field(3, NEXT_UNREAD_MSG_ID_4);
-            launcher.add_field(4, NEXT_UNREAD_MSG_ID_5);
-            launcher.add_region_requirement(RegionRequirement(
-                runtime->get_logical_subregion_by_color(
-                    channel_partition, channel_id_1[request.user_id]),
-                READ_ONLY, EXCLUSIVE, channels));
-            launcher.add_field(0, NEXT_MSG_ID);
-            launcher.add_region_requirement(RegionRequirement(
-                runtime->get_logical_subregion_by_color(
-                    channel_partition, channel_id_2[request.user_id]),
-                READ_ONLY, EXCLUSIVE, channels));
-            launcher.add_field(0, NEXT_MSG_ID);
-            launcher.add_region_requirement(RegionRequirement(
-                runtime->get_logical_subregion_by_color(
-                    channel_partition, channel_id_3[request.user_id]),
-                READ_ONLY, EXCLUSIVE, channels));
-            launcher.add_field(0, NEXT_MSG_ID);
-            launcher.add_region_requirement(RegionRequirement(
-                runtime->get_logical_subregion_by_color(
-                    channel_partition, channel_id_4[request.user_id]),
-                READ_ONLY, EXCLUSIVE, channels));
-            launcher.add_field(0, NEXT_MSG_ID);
-            launcher.add_region_requirement(RegionRequirement(
-                runtime->get_logical_subregion_by_color(
-                    channel_partition, channel_id_5[request.user_id]),
-                READ_ONLY, EXCLUSIVE, channels));
-            launcher.add_field(0, NEXT_MSG_ID);
+            launcher.add_field(0, NEXT_UNREAD_MSG_IDS);
+            for (unsigned int i = 0; i < CHANNELS_PER_USER; i++) {
+                launcher.add_region_requirement(RegionRequirement(
+                    runtime->get_logical_subregion_by_color(
+                        channel_partition, channel_id_mem[request.user_id][i]),
+                    READ_ONLY, EXCLUSIVE, channels));
+                launcher.add_field(0, NEXT_MSG_ID);
+            }
             pending_reqs.push_back(
                 {.future = runtime->execute_task(ctx, launcher),
                  .request = request});

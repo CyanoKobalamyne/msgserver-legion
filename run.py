2e031c0c96f3e54fa325ad8a6374686d9ea2ce13
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2020-05-19 23:47:20 -0400

    Fix tried channel counts

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import os.path
 ITERATIONS = 30
 
 USERS = [1, 2, 5, 10, 20, 50, 100]
-CHANNELS = [1, 2, 3, 4, 5, 10]
+CHANNELS = [5, 10, 20, 50, 100]
 MESSAGES = [500]
 
 REQUESTS = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
